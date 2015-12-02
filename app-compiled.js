1a0750f809a78e843f17f8fc51350d3e6098525c
Sander Tuin <github@sandert.me>
2015-12-02 22:57:32 +0100

    es6

--- app-compiled.js
+++ app-compiled.js
@@ -1,7 +1,9 @@
-((window, document) => {
-  document.addEventListener("touchstart", () => {}, true);
+'use strict';
 
-  const App = {};
+(function (window, document) {
+  document.addEventListener("touchstart", function () {}, true);
+
+  var App = App || {};
 
   var button = document.querySelector('#lunch');
 
@@ -12,19 +14,21 @@
     username: 'Bender'
   };
 
-  var params = Object.keys(App.options).map(k => {
+  var params = Object.keys(App.options).map(function (k) {
     return encodeURIComponent(k) + '=' + encodeURIComponent(App.options[k]);
   }).join('&');
 
-  button.onclick = e => {
+  button.onclick = function (e) {
     button.innerText = 'Posting...';
-    fetch(`https://slack.com/api/chat.postMessage?${ params }`, { method: 'POST' }).then(res => {
+    fetch('https://slack.com/api/chat.postMessage?' + params, { method: 'POST' }).then(function (res) {
       if (res.ok == true) {
         button.innerText = 'Posted!';
       } else {
         button.innerText = res.error;
       }
-    }).catch(e => alert(e));
+    })['catch'](function (e) {
+      return alert(e);
+    });
     return false;
   };
 
